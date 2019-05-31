ddc8bb44d9a3dd01c7cdd674bffd324cfbfc3011
Rahul Sharma <rshar24@gmail.com>
2019-05-31 15:39:04 +0800

    rating on show page

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,4 @@
 class Location < ApplicationRecord
-
   belongs_to :user
   has_many :photos
   has_many :reviews
@@ -20,4 +19,39 @@ class Location < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  def avg_rating
+    total = self.reviews.count
+    total_sum = 0.0
+    if self.reviews.exists?
+      self.reviews.each do |review|
+        total_sum += review.rating
+      end
+      raw_rating = total_sum / total
+      (raw_rating * 2).round.to_f / 2
+    else
+      0
+    end
+  end
+
+  def avg_rating_dec
+    total = self.reviews.count
+    total_sum = 0.0
+    if self.reviews.exists?
+      self.reviews.each do |review|
+        total_sum += review.rating
+      end
+      (total_sum.to_f / total).round(1)
+    else
+      return 0.0
+    end
+  end
+
+  def reviews_count
+    if self.reviews.count.nil?
+      return 0
+    else
+      return self.reviews.count
+    end
+  end
 end
